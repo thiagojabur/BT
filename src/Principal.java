6a194ce1bfc649dd57c7aa0099266a5a742db91b
Thiago Jabur Bittar <31863285+thiagojabur@users.noreply.github.com>
2024-10-07 16:23:46 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -3,34 +3,31 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		
-		
+
 		Node cinco = new Node(5);
 		Node oito = new Node(8);
 		Node nove = new Node(9);
-		Node sete = new Node(7, oito, nove);
+		Node sete = new Node(7);
 		Node seis = new Node(6);
-		Node dois = new Node(2, seis, sete);
+		Node dois = new Node(2);
+
+		Node quatro = new Node(4);
+		Node tres = new Node(3);
+		Node raiz = new Node(1);
+		
+		BinaryTree teste = new BinaryTree(cinco);
 		
 		
+		teste.insertNode(tres);
+		teste.insertNode(dois);
 		
-		Node quatro = new Node(4, null, null);
-		Node tres = new Node(3, null, quatro);
-		Node raiz = new Node(1, null, tres);
-		
-		BinaryTree teste = new BinaryTree(raiz);
-		
-		teste.addLeftChild(raiz, dois);
-		teste.addLeftChild(tres, cinco);
-		
-		System.out.println(teste.isLeaf(raiz));
-		System.out.println(teste.isInternal(raiz));
-		System.out.println("Pai: " +teste.getParent(tres).getValue());
-		System.out.println("Nível: " +teste.depth(raiz));
+		//System.out.println(teste.isLeaf(raiz));
+		//System.out.println(teste.isInternal(raiz));
+		//System.out.println("Nível: " +teste.depth(raiz));
 	    
-		teste.printPreOrder();
+		teste.printInOrder();
 		
-		System.out.println(teste.isStrictBinaryTree());
+		//System.out.println(teste.isStrictBinaryTree());
 		
 		System.out.println("Folhas");
 		teste.printLeafs();
@@ -40,7 +37,11 @@ public class Principal {
 		
 		System.out.println("É full (cheia)?");
 		System.out.println(teste.isFullBinaryTree());
-		teste.printPostOrder();
+		//teste.printPostOrder();
 	
+		teste.deleteNode(dois);
+		
+		teste.printInOrder();
+		
 	}
 }
